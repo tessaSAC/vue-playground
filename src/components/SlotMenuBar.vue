7337974d758d347f42044dd57fb6b37e4e2c5383
tessaSAC <tessaSAC@users.noreply.github.com>
2019-02-12 11:15:41 -0500

    create reusable slot menu bar example

---
+++ src/components/SlotMenuBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="SlotMenuBar">
+    <div class="left">
+      <slot name="left" />
+    </div>
+
+    <div class="right">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.SlotMenuBar {
+  width: 100%;
+  background-color: thistle;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .left, .right {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="Home">
+    <SlotMenuBar>
+      <template slot="left">
+        <div class="menuItem khaki" />
+        <div class="menuItem papayaWhip" />
+      </template>
+
+      <template slot="right">
+        <div class="menuItem lightSalmon" />
+        <div class="menuItem coral" />
+        <div class="menuItem tomato" />
+      </template>
+    </SlotMenuBar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import SlotMenuBar from "@/components/SlotMenuBar";
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
+    SlotMenuBar
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.Home {
+  .SlotMenuBar {
+    height: 5vh;
+  }
+
+  .menuItem {
+    width: 5vw;
+    height: 4vh;
+  }
+
+  .khaki {
+    background-color: khaki;
+  }
+  .papayaWhip {
+    background-color: papayaWhip;
+  }
+
+  .lightSalmon {
+    background-color: lightSalmon;
+  }
+  .coral {
+    background-color: coral;
+  }
+  .tomato {
+    background-color: tomato;
+  }
+}
+</style>
