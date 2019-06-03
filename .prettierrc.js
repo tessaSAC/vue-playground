2d50972695b06d82aaa046e1dda16e7637889e90
tessaSAC <tessaSAC@users.noreply.github.com>
2019-06-03 19:22:13 -0400

    updates linter settings and moves them out of package.json

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'strict',
+  jsxBracketSameLine: false,
+  proseWrap: 'never',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false
+}
