eb5fa8e365ea4892eef82f5183ffc723c764726d
tessa <tessaSAC@users.noreply.github.com>
2019-10-31 16:53:42 -0400

    start canvas experimenting

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,27 @@
+<script>
+import confetti from 'canvas-confetti'
+
+export default {
+  mounted() {
+    this.$refs.canvas.getContext('2d').fillSTyle = 'red'
+    confetti.create(this.$refs.canvas, { resize: false })
+    confetti()
+  },
+}
+</script>
+
 <template>
-  <div class="about"><h1>This is an about page</h1></div>
+<div class="About">
+  <h1>This is an about page</h1>
+  <canvas ref="canvas" width="200" height="200" fillStyle="red" />
+</div>
 </template>
+
+<style lang="scss">
+.About {
+  // background: slateblue;
+  .canvas {
+    background: red;
+  }
+}
+</style>
